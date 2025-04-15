825b6378960e2d89177ea09071dd47bdbdf1d6f4
Aleksandr <aleksandr.kalinkin62@mail.ru>
2025-04-15 21:37:54 +0300

    Add cars page, create superuser, add db.sqlite3

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from cars.models import Cars, CarsCategory
+
+admin.site.register(Cars)
+admin.site.register(CarsCategory)
--- cars/models.py
+++ cars/models.py
@@ -4,15 +4,20 @@ from django.db import models
 
 # модель = таблица
 
-# class CarsCategory(models.Model):
-#     name = models.CharField(max_length = 64, unique=True) #название категории уникально - поэтому True
-#     description = models.TextField(blank=True)# Здесь blank=True,это значит, что поле description м.б и не заполненым
-#     #Добавь прайс еще
-#
-# class Cars(models.Model):
-#     name = models.CharField(max_length = 256)
-#     # image = models.ImageField(upload_to="cars_media") #из папки cars_media будут грузиться фотки
-#     description = models.TextField(blank=True)  # Здесь blank=True,это значит, что поле description м.б и не заполненым
-#     short_description = models.TextField(max_length=64, blank=True)  # Здесь blank=True,это значит, что поле description м.б и не заполненым
-#     price = models.Decimal(max_digits=5, decimal_places=2) #5- так как у меня цена тачки за сутки максимум 30000.00 будет, 2 - так как.00
-#     cateqory = models.ForeignKey(CarsCategory, on_delete= models.PROTECT)#внешний ключ из таблицы
+class CarsCategory(models.Model):
+    name = models.CharField(max_length = 64, unique=True) #название категории уникально - поэтому True
+    description = models.TextField(blank=True)# Здесь blank=True,это значит, что поле description м.б и не заполненым
+
+    def __str__(self):
+        return self.name
+
+class Cars(models.Model):
+    name = models.CharField(max_length = 256)
+    image = models.ImageField(upload_to="cars_media") #из папки cars_media будут грузиться фотки
+    description = models.TextField(blank=True)  # Здесь blank=True,это значит, что поле description м.б и не заполненым
+    short_description = models.TextField(max_length=64, blank=True)  # Здесь blank=True,это значит, что поле description м.б и не заполненым
+    price = models.DecimalField(max_digits=7, decimal_places=2) #5- так как у меня цена тачки за сутки максимум 30000.00 будет, 2 - так как.00
+    category = models.ForeignKey(CarsCategory, on_delete= models.PROTECT)#внешний ключ из таблицы
+
+    def __str__(self):
+        return f"{self.name} | {self.category.name}"
